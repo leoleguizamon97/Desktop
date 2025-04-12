914d2bff8c650613f954a36e5dcf23becc692fce
anlleguizamongu <anlleguizamongu@unal.edu.co>
2025-04-12 18:19:54 -0500

    test 4

--- default.sh
+++ default.sh
@@ -370,7 +370,7 @@ EOF
 		install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg &
 		draw_spinner $! "Descargando clave"
 		
-		# echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |
+		echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |
 		sudo tee /etc/apt/sources.list.d/vscode.list &
 		draw_spinner $! "Agregando repositorio"
 		
